a17da3abc00c1b10cf04c09692d3212987f97316
Daniel Mackay <danielmackay@daniels-mbp.flowtrinidad.com>
2014-08-20 18:01:56 -0400

    The second commit

---
+++ json.js
@@ -0,0 +1,22 @@
+var jsonData = {
+
+	"The Heroes":
+		[
+			{
+				'heroFirstName': 'Dan',
+				'heroType': 'Knight',
+				'heroAge':   26
+			},
+			
+			{
+				'heroFirstName': 'John',
+				'heroType': 'Samurai',
+				'heroAge':   32
+			},
+			{
+				'heroFirstName': 'Nick',
+				'heroType': 'Mage',
+				'heroAge':   30
+			},
+		]
+};
--- mackay_daniel_sdiproject3.js
+++ mackay_daniel_sdiproject3.js
@@ -1,1 +1,27 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+//Daniel Mackay
+// SDI Project 3 1408
+
+// Global Variables
+
+var farmAnimals = [ 'Cows', 'Goats', 'Sheep', 'Dogs', 'Chickens'];
+var numberVillagers = 200;
+var monster1 = 'Cyclops';
+var monster2 = 'Vampire';
+var monster3 = 'Dragon';
+var monster4 = 'Giant';
+var confirm2 = confirm('Warn Villagers');
+
+console.log('It is a fine sunny day, everyone is going about their business until they see a ' + monster4 + ' rustling the bushes' + '.');
+
+// First function or Procedure
+
+var warnVillage = function(argArray){
+	if (confirm2 === true){
+	console.log('The villagers sound the alarm and prepare for attack');
+	} else ;{
+	console.log('The ' + argArray + ' are under attack' + '.')
+	}
+};
+warnVillage(farmAnimals);
+
